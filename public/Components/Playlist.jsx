8919071322c8d3cca65a7695f028f467fd44f1d3
kishanraj04 <kishuraj1111@gmail.com>
2024-10-24 19:12:48 +0530

    adding previous song play

--- public/Components/Playlist.jsx
+++ public/Components/Playlist.jsx
@@ -11,21 +11,37 @@ import SongPattern from "./SongPattern";
 export default function Playlist() {
   const playlist = JSON.parse(localStorage.getItem("playlist")) || [];
   const [isPlaying, setIsPlaying] = useState(false);
+  const [index_of_current_play_song, setIndex_Of_Current_Song] = useState(null);
   const [songImg, setSongImg] = useState({});
   const audioRef = useRef();
+
+  const handlePreviousPlay = () => {
+
+    if(index_of_current_play_song<0) return
+    const prev_song_obj = playlist[index_of_current_play_song];
+    console.log(prev_song_obj);
+    const runningplayname = prev_song_obj.name; // Assuming you want to use the artist ID
+    const song = prev_song_obj.preview_url;
+    const images = prev_song_obj.album.images[0].url;
+    setSongImg({ song, images, runningplayname });
+    setIsPlaying(false);
+    setIndex_Of_Current_Song((prev=>prev-1))
+
+  };
 
   function nextSong() {
     if (songImg.runningplayname) {
       const index_of_next_song =
         playlist.findIndex((item) => item.name == songImg.runningplayname) + 1;
       const next_song_obj = playlist[index_of_next_song];
+      if (index_of_next_song > playlist.length) return;
       const runningplayname = next_song_obj.name; // Assuming you want to use the artist ID
       const song = next_song_obj.preview_url;
       const images = next_song_obj.album.images[0].url;
+      let index_of_current_song = index_of_next_song - 1;
       setSongImg({ song, images, runningplayname });
-      console.log(songImg.song);
+      setIndex_Of_Current_Song(index_of_current_song);
       setIsPlaying(false);
-
     }
   }
 
@@ -34,11 +50,15 @@ export default function Playlist() {
     const songObj = playlist.find(
       (single) => single.artists[0].id === e.target.alt
     );
+    const index_of_current_song = playlist.findIndex(
+      (single) => single.artists[0].id === e.target.alt
+    );
+    console.log(index_of_current_song);
     const runningplayname = songObj.name; // Assuming you want to use the artist ID
     const song = songObj.preview_url;
     const images = songObj.album.images[0].url;
     setSongImg({ song, images, runningplayname });
-
+    setIndex_Of_Current_Song(index_of_current_song-1);
     // Load and play the new song
     if (audioRef.current.src !== song) {
       audioRef.current.src = song;
@@ -73,14 +93,15 @@ export default function Playlist() {
     <>
       <div className="playlist-containers">
         <div className="music-player">
-          <img src={songImg.images} alt="" onClick={playPause} />
+          {
+            !songImg.images ? <h1>Play Your Song</h1> : <img src={songImg.images} alt="" onClick={playPause} />
+          }
           <audio
             ref={audioRef}
             src={songImg.song}
             onEnded={() => {
               setIsPlaying(false);
             }}
-            
           ></audio>
 
           <div className="icons">
@@ -88,7 +109,9 @@ export default function Playlist() {
               className="same"
               size={"2rem"}
               color="yellow"
-              onClick={() => {}}
+              onClick={() => {
+                handlePreviousPlay();
+              }}
             />
             {isPlaying ? (
               <FaPause size={"2rem"} onClick={playPause} color="yellow" />
